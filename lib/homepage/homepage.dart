c1165aa6083eea9e345e3eb065d31a126ccf0f4d
Manash <Dhakalmanash@gmail.com>
2021-02-27 20:56:39 +0545

    main

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -6,6 +6,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FocusNode _longituteFocusNode = new FocusNode();
+  final FocusNode _radiusFocusNode = new FocusNode();
+
+  final _homePageFormKey = GlobalKey<FormState>();
+
+  final TextEditingController _latitudeTextEditingController =
+      new TextEditingController();
+  final TextEditingController _longitudeTextEditingController =
+      new TextEditingController();
+  final TextEditingController _radiusTextEditingController =
+      new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +32,48 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(color: Colors.cyan),
             )
           ],
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _homePageFormKey,
+          child: Column(
+            children: [
+              Container(
+                child: TextFormField(
+                  controller: _latitudeTextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Latitude Value',
+                  ),
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  focusNode: _longituteFocusNode,
+                  controller: _longitudeTextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Longitude Value',
+                  ),
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  focusNode: _radiusFocusNode,
+                  controller: _radiusTextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Radius Value',
+                  ),
+                ),
+              ),
+              Container(
+                child: RaisedButton(
+                  child: Text('Sumbit'),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
