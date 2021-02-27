3a836bb2eb477bf9be7ba33f0166a72e41f41c1b
Manash <Dhakalmanash@gmail.com>
2021-02-27 20:39:43 +0545

    first part

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Scuba'),
+            Text(
+              'Divers',
+              style: TextStyle(color: Colors.cyan),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
